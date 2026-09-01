5107dd1336781f1ec03652eeaa8eabef2ef38c07
Fenysha <126053988+Fenysha@users.noreply.github.com>
2026-09-01 15:09:38 +0300

    Refine documents as a black command interface

--- astro.config.mjs
+++ astro.config.mjs
@@ -62,6 +62,10 @@ export default defineConfig({
 				},
 			],
 			customCss: ['./src/styles/custom.css'],
+			components: {
+				Sidebar: './src/components/CommandSidebar.astro',
+				PageTitle: './src/components/CommandPageTitle.astro',
+			},
 		}),
 	],
 });
